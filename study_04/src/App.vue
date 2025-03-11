b92b8f5310e1651e0334b7a89d1360ffa203628b
Jung yuri <yurity456@gmail.com>
2025-03-11 18:06:35 +0900

    뷰 연습 및 공부

---
+++ study_04/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Ex_01 from './components/Ex_01.vue';
+import Ex_02 from './components/Ex_02.vue';
+import Ex_03 from './components/Ex_03.vue';
+import Vmodel from './components/Vmodel.vue';
+</script>
+
+
+<template>
+  <div class="wrap">
+    <div>
+      <Vmodel/>
+      <hr>
+      <Ex_03/>
+      <hr>
+      <Ex_01/>
+      <hr>
+      <Ex_02/>
+    </div>
+</div>
+
+</template>
+
+
+<style scoped>
+
+</style>
